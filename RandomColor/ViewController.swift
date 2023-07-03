5f33b236ca81a9d93fac7dfc02ff60013963ed21
Manohin <111537419+Manohin@users.noreply.github.com>
2023-07-03 16:24:12 +0300

    1

--- RandomColor/ViewController.swift
+++ RandomColor/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     @IBOutlet weak var colorView: UIView!
     @IBOutlet weak var colorNumberLabel: UILabel!
     
